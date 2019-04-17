17e8666129d2a8f74db2bf712686a72499ba1f10
Savanna Dubell <savannadubell@savannas-mbp-2.uconnect.utah.edu>
2019-04-16 21:45:55 -0600

    second commit

---
+++ liri.js
@@ -0,0 +1,119 @@
+const dotenv = require('dotenv').config();
+// Create Spotify Variable 
+var Spotify = require('node-spotify-api');
+// Create Twiiter Variable 
+const Twitter = require('twitter');
+const keys = require('./keys.js');
+console.log(keys);
+var request = require('request');
+var fs = require('fs');
+
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+var omdbKey = keys.omdb.api_key;
+
+// process.argv start with [2];
+const command = process.argv[2];
+const secondCommand = process.argv[3];
+
+// switch and case section 
+switch (command) {
+    case ('my-tweets'):
+        getTweets();
+    break;
+    case ('spotify-this-song'):
+        if(secondCommand){
+            spotifyThisSong(secondCommand);
+         } else{
+            spotifyThisSong("Die a Happy Man");
+         }
+    break;
+    case ('movie-this'):
+        if(secondCommand){
+            omdb(secondCommand);
+        } else{
+            omdb("The Holiday");
+        }
+    break;
+    case ('do-what-it-says'):
+         doThing();
+    break;
+    default:
+        console.log('Not correct, Time to try again');
+};
+
+// fucntion for Twitter
+function getTweets() {
+    client.get('statuses/home_timeline', function(error, tweets, response) {
+        if(error) throw error;
+       console.log("_______");
+                    
+                    const tweets_parsed = tweets.map(word =>word.text);
+                    tweets_parsed.forEach(function(element) {
+                    console.log(element);
+                    });
+                });
+            }
+// function for Spotify 
+function spotifyThisSong(song){
+    spotify.search({ type: 'track', query: song, limit: 1}, function(error, data){
+        if(!error){
+        for(var i = 0; i < data.tracks.items.length; i++){
+            var songData = data.tracks.items[i];
+                      //artist
+            console.log("Artist: " + songData.artists[0].name);
+                      //song name
+            console.log("Song: " + songData.name);
+                      //spotify preview link
+            console.log("Preview URL: " + songData.preview_url);
+                      //album name
+            console.log("Album: " + songData.album.name);
+            console.log("-----------------------");
+            } 
+        } else {
+        console.log('Error occurred.');
+        }
+    });
+    }
+
+// function on OMDB
+    function omdb(movie){
+        var omdbURL = 'http://www.omdbapi.com/?t=' + movie + '&apikey=' + omdbKey + '&plot=short&tomatoes=true';
+    //   reguest for omdbURL
+        request(omdbURL, function (error, response, body){
+          if(!error && response.statusCode == 200){
+            var body = JSON.parse(body);
+      
+            console.log("Title: " + body.Title);
+            console.log("Release Year: " + body.Year);
+            console.log("IMdB Rating: " + body.imdbRating);
+            console.log("Country: " + body.Country);
+            console.log("Language: " + body.Language);
+            console.log("Plot: " + body.Plot);
+            console.log("Actors: " + body.Actors);
+            console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
+            console.log("Rotten Tomatoes URL: " + body.tomatoURL);
+            
+          } else{
+            console.log('Error, maybe time to try again')
+          }
+          if(movie === "The Breakfast Club"){
+            console.log("-----------------------");
+            console.log("If you haven't watched 'The Breakfast Club,' you need to!");
+            console.log("It's an American Classic");
+
+          }
+        });
+      
+      }
+
+    //   Create function for DoThing
+      function doThing(){
+        fs.readFile('random.txt', "utf8", function(error, data){
+          var txt = data.split(',');
+      
+          spotifyThisSong(txt[1]);
+        });
+      }
+    
